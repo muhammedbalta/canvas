b3f654473ef07012f5541ab69fb4486bb4c133e4
Muhammed Balta <muhammedbalta07@gmail.com>
2021-12-30 17:43:59 +0300

    first commit

---
+++ fractal-squares/script.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const maxLevel = 4;
+const branches = 2;
+let lines = []
+ctx.translate(canvas.width / 2, canvas.height / 2);
+
+//const angle = Math.PI * 2 * 0.85;
+const angle = Math.PI * 2 * 0.75;
+function drawLine(level) {
+  if (level > maxLevel) return;
+
+  ctx.strokeStyle = "#fff";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(1000, 0);
+  ctx.stroke();
+
+  for (let i = 1; i < branches + 1; i++) {
+    ctx.save();
+    ctx.translate(900 * i / (branches + 1), 0);
+    ctx.scale(0.5, 0.5);
+    ctx.save();
+
+    ctx.rotate(angle);
+    drawLine(level + 1);
+    ctx.restore();
+    ctx.save();
+
+    ctx.rotate(-angle);
+    drawLine(level + 1);
+    ctx.restore();
+
+    ctx.restore();
+  }
+}
+
+for (let i = 0; i < 4; i++) {
+  drawLine(0);
+  ctx.rotate(angle);
+}
